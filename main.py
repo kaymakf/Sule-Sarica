97991f91bc641d07a9e8ccae35e2b8bd852e52db
Furkan Kaymak <f.kaymakk@gmail.com>
2018-01-28 18:37:22 +0300

    .

--- main.py
+++ main.py
@@ -27,6 +27,19 @@ def fareySum(f1, f2):
     return Fraction(f1.numerator + f2.numerator, f1.denominator + f2.denominator)
 
 
+class Fr(Fraction):
+    def __repr__(self):
+        return '(%s/%s)' % (self.numerator, self.denominator)
+
+
+def fareySeq(n, length=False):
+    if not length:
+        return [Fr(0, 1)] + sorted({Fr(m, k) for k in range(1, n + 1) for m in range(1, k + 1)})
+    else:
+        return (n * (n + 3)) // 2 - sum(fareySeq(n // k, True) for k in range(2, n + 1))
+
+
+
 
 
 
@@ -51,62 +64,49 @@ n = simpleContinuedFraction(f)
 
 
 
-
-
-
-
-
-
-
-
-'''
-numOfNodes = 0
-for val in n:
-    numOfNodes += val
-
-#adjMatrix = [[0 for x in range(numOfNodes)] for y in range(numOfNodes)]
-'''
-
-
-
-'''
 G = nx.Graph()
-numOfNodes = 0
+numOfNodes = 2
 
 for val in n:
     numOfNodes += val
 
-secPrevNode = G.add_node(math.inf)
-prevNode = G.add_node(Fraction(0, 1))
+'''
+G.add_node(math.inf)
+G.add_node(Fraction(0, 1))
+G.add_edge(math.inf, 0)
+'''
 
-for i in range(numOfNodes - 1):
-    G.add_node(-1)
+for i in range(-1, -numOfNodes-1, -1):
+    G.add_node(i)
+
+G.add_edge(-1, -2)
+
+c = -2
+i = 1
+while i < len(n):
+    G.add_edge(c, c-n[i])
+    c -= n[i]
+    i += 1
+
+c = -3
+pb = -1
+b = -2
+for i in range(len(n)-1):
+    G.add_edge(pb, c)
+    for j in range(n[i+1]-1):
+        G.add_edge(c, b)
+        G.add_edge(c, c+1)
+        G.add_edge(c, c-1)
+        c -= 1
+    c -= 1
+    pb = b
+    b -= n[i+1]
 
 
 
-currentNode = G.add_node(f)
-#G.add_edge(math.inf, 0)
-
-for i in range(numOfNodes-1):
-    G.add_edge(prevNode, secPrevNode)
-    G.add_edge(currentNode, prevNode)
-
-
-
-
-
-#
-for val in n:
-    if val != 0:
-        G.add_node(Fraction(1, val))
-    else:
-        G.add_node(val)
-        G.add_edge(math.inf, val)
 
 
 
 
 nx.draw(G, with_labels=True, font_weight='bold')
 plt.show()
-
-'''
