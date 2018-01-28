7d80e4581fcf58fe8f3bd158fa640b7ea98ebc02
Furkan Kaymak <f.kaymakk@gmail.com>
2018-01-28 21:53:50 +0300

    .

--- main.py
+++ main.py
@@ -27,22 +27,60 @@ def fareySum(f1, f2):
     return Fraction(f1.numerator + f2.numerator, f1.denominator + f2.denominator)
 
 
-class Fr(Fraction):
-    def __repr__(self):
-        return '(%s/%s)' % (self.numerator, self.denominator)
+def findThePath(n):
+    G = nx.Graph()
+    numOfNodes = 2
 
+    for val in n:
+        numOfNodes += val
 
-def fareySeq(n, length=False):
-    if not length:
-        return [Fr(0, 1)] + sorted({Fr(m, k) for k in range(1, n + 1) for m in range(1, k + 1)})
-    else:
-        return (n * (n + 3)) // 2 - sum(fareySeq(n // k, True) for k in range(2, n + 1))
+    for i in range(-1, -numOfNodes - 1, -1):
+        G.add_node(i)
 
+    G.add_edge(-1, -2)
 
+    c = -2
+    i = 1
+    while i < len(n):
+        G.add_edge(c, c - n[i])
+        c -= n[i]
+        i += 1
 
+    c = -3
+    pb = -1
+    b = -2
+    for i in range(len(n) - 1):
+        G.add_edge(pb, c)
+        for j in range(n[i + 1] - 1):
+            G.add_edge(c, b)
+            G.add_edge(c, c + 1)
+            G.add_edge(c, c - 1)
+            c -= 1
+        c -= 1
+        pb = b
+        b -= n[i + 1]
 
+    G.nodes[-1]['value'] = math.inf
+    G.nodes[-2]['value'] = Fraction(0, 1)
+    G.nodes[-3]['value'] = Fraction(1, 1)
 
+    for i in range(-4, -numOfNodes - 1, -1):
+        adjList = sorted(list(G.neighbors(i)), reverse=True)
+        G.nodes[i]['value'] = fareySum(G.nodes[adjList[0]]['value'], G.nodes[adjList[1]]['value'])
 
+    if G.nodes[-numOfNodes]['value'] != f:
+        print("something is wronh!!!")
+
+    #nx.draw(G, with_labels=True, font_weight='bold')
+    #plt.show()
+
+    shortestPath = nx.dijkstra_path(G, -1, -numOfNodes)
+    shortestPathValues = []
+
+    for i in shortestPath:
+        shortestPathValues.append(G.nodes[i]['value'])
+
+    return shortestPathValues
 
 
 
@@ -55,61 +93,16 @@ while True:
         print("Wrong input.\n")
 
 f = Fraction(int(inp.split('/')[0]), int(inp.split('/')[1]))
-
 f = normalizeFraction(f)
 
-print(simpleContinuedFraction(f))
+#print(simpleContinuedFraction(f))
 
 n = simpleContinuedFraction(f)
 
+sp = findThePath(n)
+
+#print(sp)
 
 
-G = nx.Graph()
-numOfNodes = 2
-
-for val in n:
-    numOfNodes += val
-
-'''
-G.add_node(math.inf)
-G.add_node(Fraction(0, 1))
-G.add_edge(math.inf, 0)
-'''
-
-for i in range(-1, -numOfNodes-1, -1):
-    G.add_node(i)
-
-G.add_edge(-1, -2)
-
-c = -2
-i = 1
-while i < len(n):
-    G.add_edge(c, c-n[i])
-    c -= n[i]
-    i += 1
-
-c = -3
-pb = -1
-b = -2
-for i in range(len(n)-1):
-    G.add_edge(pb, c)
-    for j in range(n[i+1]-1):
-        G.add_edge(c, b)
-        G.add_edge(c, c+1)
-        G.add_edge(c, c-1)
-        c -= 1
-    c -= 1
-    pb = b
-    b -= n[i+1]
-
-G.nodes[-1]['value'] = math.inf
-G.nodes[-2]['value'] = Fraction(0, 1)
-G.nodes[-3]['value'] = Fraction(1, 1)
-
-for i in range(-4, -numOfNodes-1, -1):
-    liste = sorted(list(G.neighbors(i)), reverse=True)  # list(G.neighbors(i))
-    G.nodes[i]['value'] = fareySum(G.nodes[liste[0]]['value'], G.nodes[liste[1]]['value'])
 
 
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.show()
