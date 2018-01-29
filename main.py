ceb9973e1e9f4da1695d3ba261031faaa4626350
Furkan Kaymak <f.kaymakk@gmail.com>
2018-01-29 23:23:35 +0300

    .

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+from sympy.solvers import solve
+from sympy import Symbol
+from sympy import sympify
+from sympy.core.sympify import kernS
 
 
 def normalizeFraction(f):
@@ -74,13 +78,41 @@ def findThePath(n):
     #nx.draw(G, with_labels=True, font_weight='bold')
     #plt.show()
 
-    shortestPath = nx.dijkstra_path(G, -1, -numOfNodes)
+    shortestPaths = list(nx.all_shortest_paths(G, -1, -numOfNodes))
     shortestPathValues = []
+    spaths = []
 
-    for i in shortestPath:
-        shortestPathValues.append(G.nodes[i]['value'])
+    for j in range(len(shortestPaths)):
+        for i in shortestPaths[j]:
+            shortestPathValues.append(G.nodes[i]['value'])
+        spaths.append(shortestPathValues.copy())
+        shortestPathValues.clear()
 
-    return shortestPathValues
+    return spaths
+
+
+def tamsayiSurekliKesri(p):
+    c = [p[1]]
+    x = Symbol('x')
+    for i in range(1, len(p)-1):
+        eq = "(" + str(c[0])
+        for j in range(1, i):
+            eq += "-(1/(" + str(c[j])
+        eq += "-(1/(x"
+        for k in range(2*i+1): eq += ")"
+        eq += "-(" + str(p[i+1]) + ")"
+        eq = sympify(eq)
+        c.append(list(solve(eq, x)).pop())
+
+    for o in c:
+        print(o, end=", ")
+
+
+
+
+
+
+
 
 
 
@@ -95,14 +127,16 @@ while True:
 f = Fraction(int(inp.split('/')[0]), int(inp.split('/')[1]))
 f = normalizeFraction(f)
 
-#print(simpleContinuedFraction(f))
+print(simpleContinuedFraction(f))
 
 n = simpleContinuedFraction(f)
 
 sp = findThePath(n)
 
-#print(sp)
+for s in sp:
+    print(s)
 
 
+tamsayiSurekliKesri(sp[0])
 
 
